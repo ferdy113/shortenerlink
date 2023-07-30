ebaaafab794c1d5f16ecbefa2d78549254e3b5fc
ferdy113 <140957381+ferdy113@users.noreply.github.com>
2023-07-30 20:35:06 +0700

    Update register.php

--- register.php
+++ register.php
@@ -16,7 +16,7 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register - ShortenerLink</title>
+    <title>Register</title>
     <style>
         body {
             font-family: Arial, sans-serif;
