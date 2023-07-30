80edca7e32b9d095e3af3c6ee3f778a2b0b0063d
ferdy113 <140957381+ferdy113@users.noreply.github.com>
2023-07-30 20:34:51 +0700

    login.php

--- login.php
+++ login.php
@@ -25,7 +25,7 @@ if(isset($_POST['Login'])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login - ShortenerLink</title>
+    <title>Login</title>
     <style>
         body {
             font-family: Arial, sans-serif;
